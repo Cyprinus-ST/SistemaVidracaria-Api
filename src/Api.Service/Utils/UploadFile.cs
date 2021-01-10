9e5af7eff614081ad25bd8f77597be3210a119cf
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2021-01-10 16:39:52 -0300

    Método de upload de imagem

---
+++ src/Api.Service/Utils/UploadFile.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Api.Service.Utils
+{
+    public class UploadFile
+    {
+        private readonly IConfiguration Configuration;
+
+        public UploadFile(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        ///  Método para realizar o upload de arquivos
+        /// </summary>
+        /// <param name="destinationFolder"> Pasta aonde o arquivo será salvar considero o caminho padrão src/Files/DestinantionFoleder</param>
+        /// <param name="file"></param>
+        /// <param name="idUser"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public async Task<string> Upload(string destinationFolder, IFormFile file, Guid idUser, string fileName)
+        {
+            try
+            {
+
+                string serverPath = Configuration["PathFile"];
+
+                serverPath = serverPath + "/" + idUser + "/" + destinationFolder;
+
+
+                //Se o diretório não existir ele vai criar
+                if (!Directory.Exists(serverPath))
+                {
+                    Directory.CreateDirectory(serverPath);
+                }
+
+                using (FileStream filestream = System.IO.File.Create(serverPath + @"/" + fileName))
+                {
+                    await file.CopyToAsync(filestream);
+                    filestream.Flush();
+                    string path = serverPath + "/" + fileName;
+                    return path;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
+
