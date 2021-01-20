12911d5acf012ce4ce118309894ceb41fd8527ab
Caio Duarte Santos <caio.duarte@dtidigital.com.br>
2021-01-20 16:19:22 -0300

    Authorize nas rotas de planos

--- src/Api.Application/Controllers/PlanController.cs
+++ src/Api.Application/Controllers/PlanController.cs
@@ -32,7 +32,7 @@ namespace Api.Application.Controllers
             }
         }
 
-        [Authorize]
+        [Authorize("Bearer")]
         [HttpGet]
         public async Task<ActionResult<object>> ListPlan([FromServices] IPlanService service)
         {
@@ -51,6 +51,7 @@ namespace Api.Application.Controllers
             }
         }
 
+        [Authorize("Bearer")]
         [HttpPut]
         [Route("{id}")]
         public async Task<ActionResult<object>> UpdatePlan([FromRoute] Guid id, [FromBody] UpdatePlanInput input, [FromServices] IPlanService service)
@@ -71,8 +72,8 @@ namespace Api.Application.Controllers
             }
         }
 
-        [Authorize]
-        [HttpPatch]
+        [Authorize("Bearer")]
+        [HttpDelete]
         public async Task<ActionResult<object>> DeletePlan([FromQuery] DeletePlanInput input, [FromServices] IPlanService service)
         {
             try
